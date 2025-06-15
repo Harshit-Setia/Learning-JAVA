9cd9ea0500433a191f4704518343d82364c769c5
Harshit <harshitsetia.005@gmail.com>
2025-06-15 17:04:12 +0530

    01&02

---
+++ Bacics/DataType.java
@@ -0,0 +1,29 @@
+public class DataType {
+    public static void main(String[] args) {
+        
+
+        //Integer
+        {
+        int a=10;  // 4 byte
+        long b=10; // 8 byte
+        short c=10;// 2 byte
+        byte d=10; // 1 byte
+        }
+        
+        //float
+        {
+        float a=10.1f; // 4 byte
+        double b=10.1; // 8 byte
+        }
+
+        //character
+        {
+        char a='1'; // 2 byte
+        }
+
+        //boolean
+        {
+        boolean a=true; // true or false
+        }
+    }
+}
